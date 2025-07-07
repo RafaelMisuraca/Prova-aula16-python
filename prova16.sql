b8bd75af7bd257fce2b9fed49fd360ca178878ba
RafaelMisuraca <rafaelmisuraca@gmail.com>
2025-07-06 22:07:42 -0300

    Add files via upload

---
+++ prova16.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE PROVAS;
+USE Provas;
+
+CREATE TABLE Produtos (
+	ProdutosID INT PRIMARY KEY AUTO_INCREMENT,
+    NomeProduto varchar(100) NOT NULL,
+    Quantidade INT NOT NULL,
+	Preco DECIMAL(10, 2) NOT NULL
+);
+
+INSERT INTO Produtos(ProdutosID, NomeProduto, Quantidade, Preco) VALUES
+(1, 'Notebook Dell Inspiron', 15, 4299.99),
+(2, 'Smartphone Samsung Galaxy S23', 30, 3799.00),
+(3, 'Fone de Ouvido Bluetooth Sony', 50, 399.90);
